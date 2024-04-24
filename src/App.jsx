f19758b3793d2a9ec2ae0964ddfa21b19f6e4ff9
Brandon Castro <C1brandon626@gmail.com>
2024-04-23 18:11:00 -0700

    deleted iframe import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import Map from './components/Map'
 import './App.css'
-import Iframe from 'react-iframe'
 
 
 function App() {
@@ -11,18 +10,18 @@ function App() {
 
   return (
     <>
-    {/* Title of Site */}
-    <div>
-      <h1>EasyRoute</h1>
-    </div>
-    {/* Added iframe for locally hosted map*/}
-    <div>
-      <iframe
-        width="1125px"
-        height="625px"
-        src="https://ucsc.maps.arcgis.com/apps/instant/3dviewer/index.html?appid=8fe04a1bb50141e081472ca07d8fe1f8"
-        style={{ border: 'none', display: 'block' }}></iframe>
-    </div>
+      {/* Title of Site */}
+      <div>
+        <h1>EasyRoute</h1>
+      </div>
+      {/* Added iframe for locally hosted map*/}
+      <div>
+        <iframe
+          width="1125px"
+          height="625px"
+          src="https://ucsc.maps.arcgis.com/apps/instant/3dviewer/index.html?appid=8fe04a1bb50141e081472ca07d8fe1f8"
+          style={{ border: 'none', display: 'block' }}></iframe>
+      </div>
     </>
   )
 }
